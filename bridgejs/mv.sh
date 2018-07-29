d3c4597e6a2eaf9a5f873b741233268d468e28f7
Xavier <xinliugm@gmail.com>
2018-07-29 23:09:13 +0800

    🚧WIP: remove mv.sh, add a sample in bridgesdk.ts

--- bridgejs/mv.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ../jstraw/src/main/assets
-mkdir -p ../jstraw-ext/src/main/assets
-
-cp -u ./dist/straw.bundle-$1.js ../jstraw/src/main/assets/
-cp -u ./dist/straw_ext.bundle-$1.js ../jstraw-ext/src/main/assets/
--- bridgejs/src/sdk/bridgesdk.ts
+++ bridgejs/src/sdk/bridgesdk.ts
@@ -1,8 +1,23 @@
+import Bridge from "../core/bridge";
+
 /**
  * BridgeSDK
  * this class is used wrap native service in a convient way,
  * then developers can request native service by simplely
  * calling bridgesdk.servicemethod(param).then(r=>{})
  */
-class BridgeSDK {
+const nativeServices = {
+    showToast: "toast",
+};
+function getBridge(): Bridge {
+    return (window as any).bridge as Bridge;
 }
+
+// FIXME: this is a sample
+function showToast(msg: string): Promise<string> {
+    return getBridge().callNative<string, string>(nativeServices.showToast, msg);
+}
+
+export {
+    showToast,
+};
